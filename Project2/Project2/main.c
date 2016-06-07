d82fbc9e49282d0feed3b101a4f9feb41f3d1934
Rhys Powell <rhys.g.powell@gmail.com>
2016-06-07 09:57:48 -0400

    Initial Commit

---
+++ Project2/Project2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project2
+//
+//  Created by Rhys Powell on 6/7/16.
+//  Copyright © 2016 Rhys Powell. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
